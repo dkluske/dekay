0001a227db4fe8891b7e0d8e15b1188a3081c466
Dominik Kluske <46371246+dkluske@users.noreply.github.com>
2025-01-24 08:04:32 +0100

    Update Configuration.kt

--- composeApp/src/commonMain/kotlin/io/dkluske/dekay/util/Configuration.kt
+++ composeApp/src/commonMain/kotlin/io/dkluske/dekay/util/Configuration.kt
@@ -4,5 +4,13 @@ data class Configuration(
     val name: Pair<String, String?> = "User" to null,
     val username: String = "user",
     val age: Int = 1,
-    val height: Int = 1
+    val height: Int = 1,
+    val dailyStepTarget: Int = 0,
+    val gender: Gender = Gender.NOT_DEFINED
 )
+
+enum class Gender {
+    MALE,
+    FEMALE,
+    NOT_DEFINED
+}
