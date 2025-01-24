cffaa905211d11f0d67bba7c843c88baf23c9d8a
Dominik Kluske <dome.k28@yahoo.com>
2025-01-24 05:53:54 +0100

    add initial color theme

---
+++ composeApp/src/commonMain/kotlin/io/dkluske/dekay/util/Colors.kt
@@ -0,0 +1,20 @@
+package io.dkluske.dekay.util
+
+import androidx.compose.material.Colors
+import androidx.compose.ui.graphics.Color
+
+val CUSTOM_THEME_DARK = Colors(
+    primary = Color(31, 36, 42),
+    primaryVariant = Color(31, 31, 31),
+    secondary = Color(86, 136, 255),
+    secondaryVariant = Color(86, 136, 255),
+    background = Color(31, 36, 42),
+    surface = Color(54, 54, 54),
+    error = Color(215, 0, 0),
+    onPrimary = Color(179, 179, 179),
+    onSecondary = Color(179, 179, 179),
+    onBackground = Color(179, 179, 179),
+    onSurface = Color(179, 179, 179),
+    onError = Color(179, 179, 179),
+    isLight = false
+)
