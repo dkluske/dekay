9dd283a4781af4696cef00f25f78f99e3705cc69
Dominik Kluske <dominik.kluske@proton.me>
2025-01-25 11:07:32 +0100

    add initial frame for home view

--- composeApp/src/commonMain/kotlin/io/dkluske/dekay/views/home/HomeView.kt
+++ composeApp/src/commonMain/kotlin/io/dkluske/dekay/views/home/HomeView.kt
@@ -1,10 +1,10 @@
 package io.dkluske.dekay.views.home
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import io.dkluske.dekay.util.components.CardList
+import io.dkluske.dekay.util.components.CardWithFourContents
+import io.dkluske.dekay.util.components.PaddedMaxWidthRow
 import io.dkluske.dekay.views.UI
 import io.dkluske.dekay.views.ViewWrapper
 
@@ -13,9 +13,49 @@ fun HomeView(
     ui: UI
 ) {
     ViewWrapper {
-        Row(Modifier.fillMaxWidth()) {
+        PaddedMaxWidthRow {
             Text(
                 text = "Hi ${ui.configuration.value.username}"
+            )
+        }
+        PaddedMaxWidthRow {
+            // TODO: Add Health Data #4
+            CardWithFourContents(
+                upLeft = { Text(text = "upLeft") },
+                upRight = { Text(text = "upRight") },
+                downLeft = { Text(text = "downLeft") },
+                downRight = { Text(text = "downRight") }
+            )
+        }
+        PaddedMaxWidthRow {
+            // TODO: Add Workout Data #4
+            val mockupData = listOf(
+                "Workout 1",
+                "Workout 2",
+                "Workout 3",
+                "Workout 4",
+                "Workout 5",
+                "Workout 6",
+                "Workout 7",
+            )
+            CardList(
+                itemList = mockupData,
+                supplier = { it }
+            )
+        }
+
+        PaddedMaxWidthRow {
+            // TODO: Add Meal Data #4
+            val mockupData = listOf(
+                "Meal 1",
+                "Meal 2",
+                "Meal 3",
+                "Meal 4",
+                "Meal 5",
+            )
+            CardList(
+                itemList = mockupData,
+                supplier = { it }
             )
         }
     }
