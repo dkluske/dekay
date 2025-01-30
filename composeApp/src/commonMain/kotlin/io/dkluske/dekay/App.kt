36c01a0c18b2de4488e02df099a3ffc53c7e098a
Dominik Kluske <dome.k28@yahoo.com>
2025-01-30 05:57:06 +0100

    add icons and linkage for taskbar views

--- composeApp/src/commonMain/kotlin/io/dkluske/dekay/App.kt
+++ composeApp/src/commonMain/kotlin/io/dkluske/dekay/App.kt
@@ -1,12 +1,22 @@
 package io.dkluske.dekay
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -14,7 +24,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import io.dkluske.dekay.util.CUSTOM_THEME_DARK
 import io.dkluske.dekay.util.components.Card
-import io.dkluske.dekay.util.components.CardText
 import io.dkluske.dekay.views.UI
 import io.dkluske.dekay.views.View
 import io.dkluske.dekay.views.home.HomeView
@@ -47,9 +56,39 @@ fun App() {
                         ) {
                             // TODO: add real taskbar content
                             Card {
-                                CardText(
-                                    text = "Taskbar here"
-                                )
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.SpaceAround
+                                ) {
+                                    IconButton(
+                                        onClick = {
+                                            ui.state.value = View.Home()
+                                        }
+                                    ) {
+                                        Icon(Icons.Default.Home, "home")
+                                    }
+                                    IconButton(
+                                        onClick = {
+                                            ui.state.value = View.Habits()
+                                        }
+                                    ) {
+                                        Icon(Icons.Default.Add, "habits")
+                                    }
+                                    IconButton(
+                                        onClick = {
+                                            ui.state.value = View.Charts()
+                                        }
+                                    ) {
+                                        Icon(Icons.Default.Done, "charts")
+                                    }
+                                    IconButton(
+                                        onClick = {
+                                            ui.state.value = View.Settings()
+                                        }
+                                    ) {
+                                        Icon(Icons.Default.Settings, "settings")
+                                    }
+                                }
                             }
                         }
                     },
