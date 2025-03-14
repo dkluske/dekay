74243b81672ea3b3ddec4e74ef31671e2783cc68
Dominik Kluske <dome.k28@yahoo.com>
2025-03-14 11:07:59 +0100

    #1 - update imports

--- composeApp/src/androidMain/kotlin/io/dkluske/dekay/store/database/Driver.kt
+++ composeApp/src/androidMain/kotlin/io/dkluske/dekay/store/database/Driver.kt
@@ -3,10 +3,10 @@ package io.dkluske.dekay.store.database
 import android.content.Context
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
-import io.dkluske.dekay.Database
+import io.dkluske.dekay.database.Database
 
 actual class DriverFactory(private val context: Context) {
     actual fun createDriver(dbName: String): SqlDriver {
-        return AndroidSqliteDriver(Database.Schema, context, dbName)
+        return AndroidSqliteDriver(Database.Schema, context, "$dbName.db")
     }
 }
