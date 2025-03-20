79c4acd5579e8833588b487322cbbe28e3d13b40
Dominik Kluske <dome.k28@yahoo.com>
2025-03-20 06:47:56 +0100

    #1 - add settings data class + builder for init view

---
+++ composeApp/src/commonMain/kotlin/io/dkluske/dekay/store/model/Settings.kt
@@ -0,0 +1,55 @@
+package io.dkluske.dekay.store.model
+
+import kotlinx.datetime.LocalDate
+
+data class Settings(
+    val index: Int,
+    val firstName: String,
+    val lastName: String,
+    val nickName: String?,
+    val dailyStepTarget: Int?,
+    val gender: Gender,
+    val dateOfBirth: LocalDate?,
+    val height: Int?
+) {
+    enum class Gender {
+        MALE,
+        FEMALE,
+        NOT_DEFINED;
+    }
+}
+
+class SettingsBuilder {
+    var firstName = ""
+    var lastName = ""
+    var nickName: String? = null
+    var dailyStepTarget: Int? = null
+    var gender = Settings.Gender.NOT_DEFINED
+    var dateOfBirth: LocalDate? = null
+    var height: Int? = null
+
+    fun firstName(firstName: String) = apply { this.firstName = firstName }
+
+    fun lastName(lastName: String) = apply { this.lastName = lastName }
+
+    fun nickName(nickName: String?) = apply { this.nickName = nickName }
+
+    fun dailyStepTarget(dailyStepTarget: Int?) = apply { this.dailyStepTarget = dailyStepTarget }
+
+    fun gender(gender: Settings.Gender) = apply { this.gender = gender }
+
+    fun dateOfBirth(dateOfBirth: LocalDate?) = apply { this.dateOfBirth = dateOfBirth }
+
+    fun height(height: Int?) = apply { this.height = height }
+
+    fun build() = Settings(
+        index = 0,
+        firstName = firstName,
+        lastName = lastName,
+        nickName = nickName,
+        dailyStepTarget = dailyStepTarget,
+        gender = gender,
+        dateOfBirth = dateOfBirth,
+        height = height
+    )
+}
