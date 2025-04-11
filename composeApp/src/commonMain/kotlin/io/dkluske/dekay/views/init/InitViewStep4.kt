160bcba8924d95598cdc739a7b65d1faed144fa8
Dominik Kluske <dome.k28@yahoo.com>
2025-04-11 05:59:54 +0200

    #16 - add gender selection

--- composeApp/src/commonMain/kotlin/io/dkluske/dekay/views/init/InitViewStep4.kt
+++ composeApp/src/commonMain/kotlin/io/dkluske/dekay/views/init/InitViewStep4.kt
@@ -1,11 +1,25 @@
 package io.dkluske.dekay.views.init
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import io.dkluske.dekay.store.model.Settings
 import io.dkluske.dekay.store.model.SettingsBuilder
@@ -31,13 +45,91 @@ fun Steppable.InitViewStep4(
             modifier = Modifier.fillMaxWidth()
                 .padding(8.dp)
         ) {
-            // TODO: number formatting
-            InputButton(
-                displayValue = "${ui.texts.value.steps}",
-                onClick = {
-                    // TODO: Step goal picker
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    GenderButton(
+                        text = ui.texts.value.male,
+                        selected = gender.value == Settings.Gender.MALE,
+                    ) {
+                        gender.value = Settings.Gender.MALE
+                    }
                 }
-            )
+                Column(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    GenderButton(
+                        text = ui.texts.value.female,
+                        selected = gender.value == Settings.Gender.FEMALE,
+                    ) {
+                        gender.value = Settings.Gender.FEMALE
+                    }
+                }
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                GenderButton(
+                    text = ui.texts.value.notDefined,
+                    selected = gender.value == Settings.Gender.NOT_DEFINED,
+                ) {
+                    gender.value = Settings.Gender.NOT_DEFINED
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun GenderButton(
+    text: String,
+    selected: Boolean,
+    // TODO: icon
+    onClick: () -> Unit
+) {
+    val borderModifier = if (selected) {
+        Modifier.border(
+            width = 3.dp,
+            color = Color(74, 114, 170, 255),
+            shape = RoundedCornerShape(8.dp)
+        )
+    } else {
+        Modifier
+    }
+    Button(
+        onClick = onClick,
+        modifier = borderModifier.padding(8.dp).fillMaxWidth(),
+        shape = RoundedCornerShape(8.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(60, 60, 60, 200)
+        )
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.Center,
+            ) {
+                Icon(
+                    Icons.Filled.Person,
+                    "gender",
+                    modifier = Modifier.size(50.dp).padding(10.dp)
+                )
+            }
+            Row(
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = text,
+                    style = TextStyle(
+
+                    )
+                )
+            }
         }
     }
 }
