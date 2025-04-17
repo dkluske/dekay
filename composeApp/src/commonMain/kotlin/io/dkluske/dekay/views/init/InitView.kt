f33d3a1e98270746f2892581b3efd3b181974df3
Dominik Kluske <dome.k28@yahoo.com>
2025-04-17 21:19:33 +0200

    #16 - add HorizontalPager for nice swiping animation

--- composeApp/src/commonMain/kotlin/io/dkluske/dekay/views/init/InitView.kt
+++ composeApp/src/commonMain/kotlin/io/dkluske/dekay/views/init/InitView.kt
@@ -1,33 +1,30 @@
 package io.dkluske.dekay.views.init
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import io.dkluske.dekay.store.model.Settings
 import io.dkluske.dekay.store.model.SettingsBuilder
 import io.dkluske.dekay.util.Configuration
 import io.dkluske.dekay.views.UI
 import io.dkluske.dekay.views.View
 import io.dkluske.dekay.views.WithUI
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import kotlinx.datetime.LocalDate
 
 @Composable
 fun WithUI.InitView() {
-    val index = remember { mutableStateOf(0) }
     val stepMax = 8
-    val steppable = object : Steppable {
-        override val ui: UI
-            get() = this@InitView.ui
-
-        override fun previous() {
-            index.value = index.value.minus(1).coerceAtLeast(0)
-        }
-
-        override fun next() {
-            index.value = index.value.plus(1).coerceAtMost(stepMax)
-        }
-    }
-    var settingsBuilder = remember { mutableStateOf(SettingsBuilder()) }
+    val coroutineScope = rememberCoroutineScope()
+    val settingsBuilder = remember { mutableStateOf(SettingsBuilder()) }
+    val pages = (0..stepMax).toList()
+    val pagerState = rememberPagerState(initialPage = 0, pageCount = { pages.size })
 
     val step1BuilderFun: SettingsBuilder.(String, String, String?) -> Unit =
         { firstName, lastName, nickName ->
@@ -51,51 +48,77 @@ fun WithUI.InitView() {
         settingsBuilder.value = dailyStepTarget(steps)
     }
 
+    val steppable = object : Steppable {
+        override val ui: UI
+            get() = this@InitView.ui
+        override val scope: CoroutineScope
+            get() = coroutineScope
+
+        override fun previous() {
+            val backPage = pagerState.currentPage - 1
+            if (backPage >= 0) {
+                scope.launch { pagerState.animateScrollToPage(backPage) }
+            }
+        }
+
+        override fun next() {
+            val nextPage = pagerState.currentPage + 1
+            if (nextPage <= (pagerState.pageCount - 1)) {
+                scope.launch { pagerState.animateScrollToPage(nextPage) }
+            }
+        }
+    }
+
     with(steppable) {
-        when (index.value) {
-            0 -> InitViewStart()
-            
-            1 -> InitViewStep1(
-                settingsBuilder = settingsBuilder.value,
-                step1Builder = step1BuilderFun
-            )
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.fillMaxWidth()
+        ) { page ->
+            when (page) {
+                0 -> InitViewStart()
 
-            2 -> InitViewStep2(
-                settingsBuilder = settingsBuilder.value,
-                step2Builder = step2BuilderFun
-            )
-
-            3 -> InitViewStep3(
-                settingsBuilder = settingsBuilder.value,
-                step3Builder = step3BuilderFun
-            )
-
-            4 -> InitViewStep4(
-                settingsBuilder = settingsBuilder.value,
-                step4Builder = step4BuilderFun
-            )
-
-            5 -> InitViewStep5(
-                settingsBuilder = settingsBuilder.value,
-                step5Builder = step5BuilderFun
-            )
-
-            6 -> InitViewStep6()
-
-            7 -> InitViewFinish()
-
-            8 -> {
-                val settings = settingsBuilder.value.build()
-                ui.database.value.settingsQueries.update(settings.toDatabaseModel())
-                ui.configuration.value = Configuration(
-                    name = settings.firstName to settings.lastName,
-                    username = settings.nickName ?: settings.firstName,
-                    age = settings.dateOfBirth ?: LocalDate(1970, 1, 1),
-                    height = settings.height,
-                    dailyStepTarget = settings.dailyStepTarget,
-                    gender = settings.gender
+                1 -> InitViewStep1(
+                    settingsBuilder = settingsBuilder.value,
+                    step1Builder = step1BuilderFun
                 )
-                ui.state.value = View.Home()
+
+                2 -> InitViewStep2(
+                    settingsBuilder = settingsBuilder.value,
+                    step2Builder = step2BuilderFun
+                )
+
+                3 -> InitViewStep3(
+                    settingsBuilder = settingsBuilder.value,
+                    step3Builder = step3BuilderFun
+                )
+
+                4 -> InitViewStep4(
+                    settingsBuilder = settingsBuilder.value,
+                    step4Builder = step4BuilderFun
+                )
+
+                5 -> InitViewStep5(
+                    settingsBuilder = settingsBuilder.value,
+                    step5Builder = step5BuilderFun
+                )
+
+                6 -> InitViewStep6()
+
+                7 -> InitViewFinish()
+
+                8 -> {
+                    val settings = settingsBuilder.value.build()
+                    ui.database.value.settingsQueries.update(settings.toDatabaseModel())
+                    ui.configuration.value = Configuration(
+                        name = settings.firstName to settings.lastName,
+                        username = settings.nickName ?: settings.firstName,
+                        age = settings.dateOfBirth ?: LocalDate(1970, 1, 1),
+                        height = settings.height,
+                        dailyStepTarget = settings.dailyStepTarget,
+                        gender = settings.gender
+                    )
+                    ui.state.value = View.Home()
+                }
             }
         }
     }
@@ -103,6 +126,8 @@ fun WithUI.InitView() {
 
 interface Steppable {
     val ui: UI
+    val scope: CoroutineScope
+
     fun previous()
 
     fun next()
