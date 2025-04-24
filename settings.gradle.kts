399373decc313c9b8f971a1241d47507e8869a3b
Dominik Kluske <46371246+dkluske@users.noreply.github.com>
2025-04-24 10:18:37 +0200

    add github packages for healthKMP

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,6 +27,15 @@ dependencyResolutionManagement {
         }
         mavenCentral()
         maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
+        maven {
+            name = "GitHubPackagesDkluskeHealthKMP"
+            url = uri("https://maven.pkg.github.com/dkluske/HealthKMP")
+            
+            credentials {
+                username = System.getenv("GITHUB_ACTOR") ?: project.findProperty("gpr.user")?.toString()
+                password = System.getenv("GITHUB_TOKEN") ?: project.findProperty("gpr.key")?.toString()
+            }
+        }
     }
 }
 
