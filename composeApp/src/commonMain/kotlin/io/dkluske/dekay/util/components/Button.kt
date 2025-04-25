e1eb5b77fb5c55d2f504779addd623d38547f5a8
Dominik Kluske <46371246+dkluske@users.noreply.github.com>
2025-04-25 12:18:19 +0200

    #16 - use correct import

--- composeApp/src/commonMain/kotlin/io/dkluske/dekay/util/components/Button.kt
+++ composeApp/src/commonMain/kotlin/io/dkluske/dekay/util/components/Button.kt
@@ -10,10 +10,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.Text
-import androidx.compose.material3.graphics.vector.ImageVector
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 
 @Composable
