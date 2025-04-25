4b5afefa076113e4f6554598166e92875a8dd764
Dominik Kluske <46371246+dkluske@users.noreply.github.com>
2025-04-25 12:06:17 +0200

    #16 fix imports

--- composeApp/src/commonMain/kotlin/io/dkluske/dekay/util/components/Button.kt
+++ composeApp/src/commonMain/kotlin/io/dkluske/dekay/util/components/Button.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.Text
+import androidx.compose.material3.graphics.vector.ImageVector
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
