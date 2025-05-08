dda2964e884e4efb6994bcfbaa9a13a3baf02b44
Dominik Kluske <46371246+dkluske@users.noreply.github.com>
2025-05-08 09:38:43 +0200

    use remembered coroutineScope for launching the auth

--- composeApp/src/commonMain/kotlin/io/dkluske/dekay/views/init/InitViewStep6.kt
+++ composeApp/src/commonMain/kotlin/io/dkluske/dekay/views/init/InitViewStep6.kt
@@ -1,6 +1,7 @@
 package io.dkluske.dekay.views.init
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import com.viktormykhailiv.kmp.health.HealthDataType
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -9,6 +10,7 @@ import kotlinx.coroutines.launch
 
 @Composable
 fun Steppable.InitViewStep6() {
+    val scope = rememberCoroutineScope()
     InitDataStepView(
         title = ui.texts.value.initStep6Title,
         onNext = { true }
@@ -16,7 +18,7 @@ fun Steppable.InitViewStep6() {
         InputButton(
             displayValue = ui.texts.value.manageHealthAccess
         ) {
-            CoroutineScope(Dispatchers.IO).launch {
+            scope.launch {
                 ui.health.value.requestAuthorization(
                     readTypes = listOf(
                         HealthDataType.Steps,
