0006c38e1c64aab5c0906f7464e22f19d0ce462e
Dominik Kluske <46371246+dkluske@users.noreply.github.com>
2025-05-15 14:32:19 +0200

    #16 - fix pipe errors

--- composeApp/src/commonMain/kotlin/io/dkluske/dekay/views/workouts/WorkoutsDayView.kt
+++ composeApp/src/commonMain/kotlin/io/dkluske/dekay/views/workouts/WorkoutsDayView.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
@@ -66,7 +67,7 @@ fun WithUI.WorkoutsDayView() {
                 IconButton(
                     onClick = {} // TODO: calendar date picker
                 ) {
-                    Icon(Icons.Default.Today, "date")
+                    //Icon(Icons.Default.Today, "date")
                 }
             }
         }
@@ -95,7 +96,7 @@ fun WithUI.WorkoutsDayView() {
                 }
             }
         }
-        val workouts = listOf() // TODO: health pull workouts of that day
-        
+        val workouts = listOf<String>() // TODO: health pull workouts of that day
+
     }
 }
